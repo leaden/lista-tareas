a06959887cfcf7fecfe8a18b01d4efae796d7ca6
maribel <maribel.diaz.j@gmail.com>
2017-04-27 12:40:09 -0300

    recorriendo el arreglo de la variable

---
+++ assets/js/main.js
@@ -0,0 +1,80 @@
+
+var resultado = [
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "et porro tempora",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 5,
+    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 6,
+    "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 7,
+    "title": "illo expedita consequatur quia in",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 8,
+    "title": "quo adipisci enim quam ut ab",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 9,
+    "title": "molestiae perspiciatis ipsa",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 10,
+    "title": "illo est ratione doloremque quia maiores aut",
+    "completed": true
+  };
+  
+];
+//recorrer el arreglo de mi variable resultado 
+var datos = [];
+
+  for (i=0; i < 10;i++)
+{
+ datos.push(resultado[i])
+}
+
+datos.forEach(function(el){
+  document.write("<li>" + el.userId + el.id + " " + el.title + "/ " + el.completed + "</li>");
+  });
+
+
+
+
+
